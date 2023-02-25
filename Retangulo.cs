ad9515c0637d6d7a5e5481f6ad2fc831ea0fb800
Eduardo Almeida <106634774+EduuardoAlmeida@users.noreply.github.com>
2023-02-25 16:57:18 -0300

    Programa finalizado.

--- Retangulo.cs
+++ Retangulo.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 
-namespace Atividade
+namespace CalcularRetangulo
+
+internal class Retangulo
 {
-    internal class Retangulo
+    public double Largura;
+    public double Altura;
+
+    public double Area(double largura, Double altura)
     {
-        public double Largura;
-        public double Altura;
+        return largura * altura;
+    }
 
-        public double Area(double largura, Double altura)
-        {
-            return largura * altura;
-        }
+    public double Perimetro(double largura, double altura)
+    {
+        return 2*(largura + altura);
+    }
 
-        public double Perimetro(double largura, double altura)
-        {
-            return 2*(largura + altura);
-        }
+    public double Diagonal (double largura, double altura)
+    {
+        double resultado = Math.Pow(Largura, 2) + Math.Pow(Altura, 2);
 
-        public double Diagonal (double largura, double altura)
-        {
-            double resultado = Math.Pow(Largura, 2) + Math.Pow(Altura, 2);
+        resultado = Math.Sqrt(resultado);
 
-            resultado = Math.Sqrt(resultado);
-
-            return resultado;
-        }
+        return resultado;
     }
 }
