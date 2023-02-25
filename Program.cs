84c5d0402b323082a69ad00e4440b956e4f042c6
Eduardo Almeida <106634774+EduuardoAlmeida@users.noreply.github.com>
2023-02-25 16:39:23 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Qual a largura do retangulo?");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Qual a altura do retangulo?");
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = r.Area(r.Largura, r.Altura);
+
+            double perimetro = r.Perimetro(r.Largura, r.Altura);
+
+            double diagonal = r.Diagonal(r.Largura, r.Altura);
+
+            Console.WriteLine("A área do retângulo é: " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("O perímetro do retângulo é: " + perimetro.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("A diagonal do retângulo é: " + diagonal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Atividade
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area(double largura, Double altura)
+        {
+            return largura * altura;
+        }
+
+        public double Perimetro(double largura, double altura)
+        {
+            return 2*(largura + altura);
+        }
+
+        public double Diagonal (double largura, double altura)
+        {
+            double resultado = Math.Pow(Largura, 2) + Math.Pow(Altura, 2);
+
+            resultado = Math.Sqrt(resultado);
+
+            return resultado;
+        }
+    }
+}
